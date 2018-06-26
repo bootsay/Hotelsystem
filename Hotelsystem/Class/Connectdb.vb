eba2bdf300b87f80cc67e8b9c95b43a05d63d492
boot vong <Bootvong@hotmail.com>
2018-06-26 20:59:33 +0700

    Initial commit

---
+++ Hotelsystem/Class/Connectdb.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Data.SqlClient
+Public Class connectdb
+    Public conn As New SqlConnection
+    Public user As String = "sa"
+    Public password As String = "bootvong"
+    Public str As String = "data source='" & configuration.Default.servername & "'; initial catalog='" & configuration.Default.dbname & "'; Persist Security Info=True;User ID=sa;Password=bootvong"
+    Public Function connect()
+        Try
+            With conn
+                If conn.State = ConnectionState.Open Then .Close()
+                .ConnectionString = str
+                .Open()
+            End With
+
+        Catch ex As Exception
+            MessageBox.Show("ບໍ່ສາມາດເຊື່ອມຕໍ່ກັບຖານຂໍ້ມູນ, ກະລຸນາກວດເບີ່ງການເຊື່ອມຕໍ່", "", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Try
+
+        Return True
+    End Function
+
+End Class
---
+++ Hotelsystem/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Hotelsystem")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Hotelsystem")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("13155190-1364-4975-b393-b6832a133cf6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
