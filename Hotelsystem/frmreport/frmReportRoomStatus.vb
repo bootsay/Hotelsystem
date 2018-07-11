2987cc2b6839e0d26a15e09da6e0a255e202e748
bootvong <32062397+bootvong@users.noreply.github.com>
2018-07-11 12:20:26 +0700

    อับเดดละเดี้ส่วนหว

--- Hotelsystem/frmreport/frmReportRoomStatus.vb
+++ Hotelsystem/frmreport/frmReportRoomStatus.vb
@@ -1,42 +1,43 @@
 ﻿Imports System.Data.SqlClient
 Imports CrystalDecisions.CrystalReports.Engine
-Public Class frmReportprovince
+Public Class frmReportRoomStatus
     Dim cn As New connectdb
     Dim da As New SqlDataAdapter
     Dim dt, dt1, dt2 As New DataTable
     Dim cm As New SqlCommand
     Dim markettype As New tbfbookingtype
-    Dim cry As New cryReportprovince
+    Dim cry As New CryReportRoomStatus
     'Dim data1 As New mydata
     Dim dttt As New DataTable
     Public sup_id As String
+    Dim roomlocation As New tbfroomlocation
     Public frmreportsaledetail As Boolean = False
     Private Sub Button1_Click(sender As Object, e As EventArgs)
         Me.Close()
     End Sub
 
     Private Sub Frmreport_Sale_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'Try
-        '    markettype.combobookingtype(cbmarkettry)
-        '    cbmarkettry.SelectedValue = 0
-        'Catch ex As Exception
-        '    MessageBox.Show(ex.Message)
-        'End Try
+        Try
+            roomlocation.comboroomlocation(cblocation)
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
     End Sub
 
     Private Sub btncry_Click(sender As Object, e As EventArgs) Handles btncry.Click
         Try
             Dim subname As String = Nothing
-            If cbcountry.SelectedValue = 0 Then
+            If cblocation.SelectedValue = 0 Then
                 subname = "%"
             Else
-                Dim rows As DataRowView = cbcountry.SelectedItem
+                Dim rows As DataRowView = cblocation.SelectedItem
                 Dim row As DataRow = rows.Row
                 subname = row(3).ToString
             End If
             dt.Clear()
             cn.connect()
-            cm = New SqlCommand("select * from viewprovience where countryname like N'%" & subname & "%'ORDER BY countryid ASC", cn.conn)
+            cm = New SqlCommand("select * from viewroom where locationname like N'%" & subname & "%'ORDER BY roomid ASC", cn.conn)
             With da
                 .SelectCommand = cm
                 .Fill(dt)
@@ -67,20 +68,27 @@ Public Class frmReportprovince
     '    'CrystalReportViewer1.ReportSource = cry
     'End Sub
 
+    Private Sub Label2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub date1_ValueChanged(sender As Object, e As EventArgs)
+
+    End Sub
 
     Private Sub ButtonX1_Click(sender As Object, e As EventArgs) Handles ButtonX1.Click
         Try
             Dim subname As String = Nothing
-            If cbcountry.SelectedValue = 0 Then
+            If cblocation.SelectedValue = 0 Then
                 subname = "%"
             Else
-                Dim rows As DataRowView = cbcountry.SelectedItem
+                Dim rows As DataRowView = cblocation.SelectedItem
                 Dim row As DataRow = rows.Row
                 subname = row(3).ToString
             End If
             dt.Clear()
             cn.connect()
-            cm = New SqlCommand("select * from viewprovience ORDER BY countryid ASC", cn.conn)
+            cm = New SqlCommand("select * from viewroom ORDER BY roomid ASC", cn.conn)
             With da
                 .SelectCommand = cm
                 .Fill(dt)
