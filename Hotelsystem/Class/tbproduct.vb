3082463a38dd2b7a5f787623caec2b43bcf2abe4
bootvong <32062397+bootvong@users.noreply.github.com>
2018-07-13 14:59:10 +0700

    hh

--- Hotelsystem/Class/tbproduct.vb
+++ Hotelsystem/Class/tbproduct.vb
@@ -50,10 +50,10 @@ Public Class tbproduct
     '    Return id
     'End Function
 
-    Public Function save(productNO As Integer, proid As Integer, cateid As Integer, proname As String, unitid As Integer, pricebuy As Double, pricesale As Double, stockqty As Integer, activate As String)
+    Public Function save(productNO As String, proid As Integer, cateid As Integer, proname As String, unitid As Integer, pricebuy As Double, pricesale As Double, stockqty As Integer, activate As String)
         cn.connect()
         Try
-            cm = New SqlCommand("insert into tbproduct(productNO,proid,cateid,proname,unitid,pricebuy,pricesale,stockqty,remark)values('" & productNO & "','" & proid & "','" & cateid & "','" & proname & "','" & unitid & "','" & pricebuy & "','" & pricesale & "','" & stockqty & "','" & activate & "')", cn.conn)
+            cm = New SqlCommand("insert into tbproduct(productNO,proid,cateid,proname,unitid,pricebuy,pricesale,stockqty,activate)values('" & productNO & "','" & proid & "','" & cateid & "',N'" & proname & "','" & unitid & "','" & pricebuy & "','" & pricesale & "','" & stockqty & "','" & activate & "')", cn.conn)
             If MessageBox.Show("ທ່ານຕ້ອງການບັນທືກແທ້ບໍ່", "ບັນທືກ", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) = DialogResult.OK Then
                 cm.ExecuteNonQuery()
             Else
@@ -80,10 +80,10 @@ Public Class tbproduct
         Return True
     End Function
 
-    Public Function update(productNO As Integer, proid As Integer, cateid As Integer, proname As String, unitid As Integer, pricebuy As Double, pricesale As Double, stockqty As Integer, activate As String)
+    Public Function update(productNO As String, proid As Integer, cateid As Integer, proname As String, unitid As Integer, pricebuy As Double, pricesale As Double, stockqty As Integer, activate As String)
         cn.connect()
         Try
-            cm = New SqlCommand("update tbproduct set proid='" & proid & "', cateid='" & cateid & "', proname='" & proname & "', unitid='" & unitid & "', pricebuy='" & pricebuy & "', pricesale='" & pricesale & "', stockqty='" & stockqty & "', activate='" & activate & "' where productNO='" & productNO & "'", cn.conn)
+            cm = New SqlCommand("update tbproduct set proid='" & proid & "', cateid='" & cateid & "', proname=N'" & proname & "', unitid='" & unitid & "', pricebuy='" & pricebuy & "', pricesale='" & pricesale & "', stockqty='" & stockqty & "', activate='" & activate & "' where productNO='" & productNO & "'", cn.conn)
             If MessageBox.Show("ທ່ານຕ້ອງການແກ້ໄຂແທ້ບໍ່", "ແກ້ໄຂ", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) = DialogResult.OK Then
                 cm.ExecuteNonQuery()
             Else
